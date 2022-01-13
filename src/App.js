f76217a5f233cd0a8154ecae608d5c795a71db81
John Oncher <you@example.comjohnonhcer@gmail.com>
2022-01-12 19:33:59 -0500

    set up content switch and state variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-
+/* eslint-disable default-case */
+import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
+  const [progress, setProgress] = useState('getUpload')
+  const [url, setUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const content = () => {
+    switch (progress) {
+        case 'getUpload':
+            return <div>PLease upload image</div>
+        case 'uploading':
+            return <h2>Uploading....</h2>;
+        case 'uploaded':
+            return <img src={url} alt="uploaded" />;
+        case 'uploadError':
+            return (
+                <>
+                    <div>Error message = {errorMessage}</div>
+                    <div>please upload image</div>
+                </>
+            );
+    }
+};
+
   return(
-    <div>
+    <div className="App" >
       <h1>Upload a picture of oreo or milo the cat</h1>
+      {content()}
     </div>
   )
-}
+  }
 
-export default App 
+export default App;
